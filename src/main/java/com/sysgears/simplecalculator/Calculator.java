fb81acfcbe5e83e88b76de505f188cb96bab2ae6
YuriHeiko <yuriheiko@gmail.com>
2017-06-28 09:42:40 +0300

    Small issues

--- src/main/java/com/sysgears/simplecalculator/Calculator.java
+++ src/main/java/com/sysgears/simplecalculator/Calculator.java
@@ -12,7 +12,8 @@ public class Calculator {
             final String PROMPT_STRING = "Enter an expression to evaluate or 'exit' to exit";
             final String VALUE_STRING = "Evaluated value is ";
             UIController uiController = new ConsoleController();
-
+            // \-?\d+(\.{1}\d+)?[\^\/\*\-\+]{1}\-?\d+(\.{1}\d+)?
+            // \({1}((\-?\d+(\.{1}\d+)?)|((\-?\d+(\.{1}\d+)?[\^\/\*\-\+]{1})+\-?\d+(\.{1}\d+)?)){1}\){1}
             // (-(12-7)*(6-2)+4/(7-3))*9
             // ((12-7)*(6-2)+4/(7-3))*9-1000*2+(10-10)
             String line;
